8cd235333a1f13d7b0e559c5dff5871167107e60
dryzxmods <dryzxmods@gmail.com>
2025-10-16 12:32:08 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+ /*  
+ * this code was created with assistance from chatgpt  
+ * feature logic developed by kyuurzy
+ */
+ 
+module.exports = {
+    apiId: Api_Id_lu,
+    apiHash: "1e9eccef56525c7ed41b2aa9c094e1ee",
+    sessionFile: "session.json",
+    ownerId: 8157933109,
+};
